ed434c33a9dbb760368bfa5eef9dd7e3c1e4125a
saifhossam <saifhossamaldin@gmail.com>
2025-05-10 20:26:52 +0300

    FavGen(need edits)

--- src/Application/Common/Models/CategoryDto.cs
+++ src/Application/Common/Models/CategoryDto.cs
@@ -1,25 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AspireApp.Application.Common.Models;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Collections.Generic;
+using AutoMapper;
 
-namespace AspireApp.Domain.Entities;
-public class CategoryDto
+namespace AspireApp.Domain.Entities
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-
-    public class Profiling : Profile
+    public class CategoryDto
     {
-        public Profiling()
-        {
-            // Entity to DTO
-            CreateMap<Category, CategoryDto>();
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
 
+        public class Profiling : Profile
+        {
+            public Profiling()
+            {
+                // Entity to DTO mapping
+                CreateMap<Category, CategoryDto>();
+            }
         }
     }
-
 }
