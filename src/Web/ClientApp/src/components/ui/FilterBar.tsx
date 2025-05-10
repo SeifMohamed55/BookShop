255edb830003bb466aedc77a970dec4d1013881e
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-05-10 22:43:17 +0300

    feat : displaying every page data

--- src/Web/ClientApp/src/components/ui/FilterBar.tsx
+++ src/Web/ClientApp/src/components/ui/FilterBar.tsx
@@ -1,30 +1,51 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { InputGroup, Input, Button } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { BookClubsClient } from "../../web-api-client";
+import { BookClub } from "../../types/interfaces/BookClubData";
 
 interface FilterBarProps {
   searchTerm: string;
   setSearchTerm: (term: string) => void;
+  valueSetter: (value: BookClub[]) => void;
 }
 
-const FilterBar: React.FC<FilterBarProps> = ({ searchTerm, setSearchTerm }) => {
-  useEffect(() => {
+const FilterBar: React.FC<FilterBarProps> = ({
+  searchTerm,
+  setSearchTerm,
+  valueSetter,
+}) => {
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
     console.log(searchTerm);
-  }, [searchTerm]);
+    const client = new BookClubsClient();
+    client
+      .searchBookClubs(searchTerm)
+      .then((res) => {
+        console.log(res);
+        res.data?.length === 0 ? valueSetter(undefined) : valueSetter(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   return (
-    <InputGroup>
-      <Input
-        type="text"
-        placeholder="Search book clubs..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="form-control"
-      />
-      <Button color="dark">
-        <FontAwesomeIcon icon={faSearch} />
-      </Button>
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup>
+        <Input
+          type="text"
+          placeholder="Search book clubs..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="form-control"
+        />
+        <Button type="submit" color="dark">
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+      </InputGroup>
+    </form>
   );
 };
 
